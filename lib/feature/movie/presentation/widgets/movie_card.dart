180668edab8bb5e64347c6268fcda168777940da
Ali <ali.ezad77@gmail.com>
2024-02-02 11:42:32 +0330

    Show card index

--- lib/feature/movie/presentation/widgets/movie_card.dart
+++ lib/feature/movie/presentation/widgets/movie_card.dart
@@ -26,19 +26,40 @@ class MovieCard extends StatelessWidget {
             onTap: () {},
             child: SizedBox(
               height: 100,
-              child: Column(
+              child: Stack(
                 children: [
-                  Expanded(
-                    child: Row(
-                      children: [
-                        if ((movie.poster ?? '').isNotEmpty)
-                          CachedNetworkImage(
-                            width: 80,
-                            height: 100,
-                            imageUrl: movie.poster ?? '',
-                            fit: BoxFit.fill,
-                            errorWidget: (context, url, error) {
-                              return SizedBox(
+                  Column(
+                    children: [
+                      Expanded(
+                        child: Row(
+                          children: [
+                            if ((movie.poster ?? '').isNotEmpty)
+                              CachedNetworkImage(
+                                width: 80,
+                                height: 100,
+                                imageUrl: movie.poster ?? '',
+                                fit: BoxFit.fill,
+                                errorWidget: (context, url, error) {
+                                  return SizedBox(
+                                    width: 80,
+                                    height: 100,
+                                    child: Center(
+                                      child: SvgPicture.asset(
+                                        'assets/svg/no_image.svg',
+                                        width: 40,
+                                        height: 40,
+                                      ),
+                                    ),
+                                  );
+                                },
+                                placeholder: (context, url) {
+                                  return const Center(
+                                    child: CupertinoActivityIndicator(),
+                                  );
+                                },
+                              )
+                            else
+                              SizedBox(
                                 width: 80,
                                 height: 100,
                                 child: Center(
@@ -48,79 +69,78 @@ class MovieCard extends StatelessWidget {
                                     height: 40,
                                   ),
                                 ),
-                              );
-                            },
-                            placeholder: (context, url) {
-                              return const Center(
-                                child: CupertinoActivityIndicator(),
-                              );
-                            },
-                          )
-                        else
-                          SizedBox(
-                            width: 80,
-                            height: 100,
-                            child: Center(
-                              child: SvgPicture.asset(
-                                'assets/svg/no_image.svg',
-                                width: 40,
-                                height: 40,
                               ),
+                            const SizedBox(
+                              width: 15,
                             ),
-                          ),
-                        const SizedBox(
-                          width: 15,
-                        ),
-                        Expanded(
-                          child: Padding(
-                            padding: const EdgeInsets.only(top: 10, bottom: 10),
-                            child: Column(
-                              mainAxisSize: MainAxisSize.max,
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Row(
+                            Expanded(
+                              child: Padding(
+                                padding:
+                                    const EdgeInsets.only(top: 10, bottom: 10),
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.max,
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
                                   children: [
-                                    Expanded(
-                                      child: AutoSizeText(
-                                        movie.title ?? '',
-                                        maxLines: 2,
-                                        style: const TextStyle(
-                                            fontFamily: 'Muli',
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: 16),
-                                      ),
+                                    Row(
+                                      children: [
+                                        Expanded(
+                                          child: AutoSizeText(
+                                            movie.title ?? '',
+                                            maxLines: 2,
+                                            style: const TextStyle(
+                                                fontFamily: 'Muli',
+                                                fontWeight: FontWeight.bold,
+                                                fontSize: 16),
+                                          ),
+                                        ),
+                                      ],
                                     ),
-                                  ],
-                                ),
-                                Row(
-                                  children: [
-                                    Text(
-                                      movie.year ?? '',
-                                      style: const TextStyle(
-                                          fontFamily: 'Muli', fontSize: 13),
-                                    ),
-                                    const SizedBox(
-                                      width: 15,
-                                    ),
-                                    const Icon(
-                                      Icons.star_rate_rounded,
-                                      size: 14,
-                                      color: Colors.orange,
-                                    ),
-                                    Text(
-                                      movie.imdbRating ?? '',
-                                      style: const TextStyle(
-                                          fontFamily: 'Muli', fontSize: 13),
+                                    Row(
+                                      children: [
+                                        Text(
+                                          movie.year ?? '',
+                                          style: const TextStyle(
+                                              fontFamily: 'Muli', fontSize: 13),
+                                        ),
+                                        const SizedBox(
+                                          width: 15,
+                                        ),
+                                        const Icon(
+                                          Icons.star_rate_rounded,
+                                          size: 14,
+                                          color: Colors.orange,
+                                        ),
+                                        Text(
+                                          movie.imdbRating ?? '',
+                                          style: const TextStyle(
+                                              fontFamily: 'Muli', fontSize: 13),
+                                        )
+                                      ],
                                     )
                                   ],
-                                )
-                              ],
+                                ),
+                              ),
                             ),
-                          ),
+                          ],
                         ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
+                  Positioned(
+                      bottom: 0,
+                      right: 0,
+                      child: Container(
+                        padding: const EdgeInsets.fromLTRB(5, 2, 5, 2),
+                          decoration: const BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(15),
+                                  bottomRight: Radius.circular(15))),
+                          child: Text(
+                            '${index + 1}',
+                            style: const TextStyle(color: Colors.black, fontFamily: 'Muli', fontSize: 12),
+                          ))),
                 ],
               ),
             ),
